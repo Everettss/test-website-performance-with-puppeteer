b7e0975b60e601bc81050c15d1690fceb6163b54
Michal Janaszek <mjanasze@intercars.eu>
2018-02-04 16:17:11 +0100

    unregister sw

--- 6/index.js
+++ 6/index.js
@@ -5,7 +5,6 @@ const testPage = require('./testPage');
   let browser = await puppeteer.launch();
   let page = await browser.newPage();
   let client = await page.target().createCDPSession();
-
   console.log(await testPage(page, client)); // first enter
   console.log(await testPage(page, client)); // second enter with cache and sw
   await browser.close();
@@ -13,17 +12,19 @@ const testPage = require('./testPage');
   browser = await puppeteer.launch();
   page = await browser.newPage();
   client = await page.target().createCDPSession();
-  await client.send('ServiceWorker.enable');
   await testPage(page, client); // only for creating fresh instance
-  await client.send('ServiceWorker.stopAllWorkers');
+  await client.send('ServiceWorker.enable');
+  await client.send('ServiceWorker.unregister', {
+    scopeURL: 'http://localhost:8080/',
+  });
   console.log(await testPage(page, client)); // second enter only with cache
   await browser.close();
 
   browser = await puppeteer.launch();
   page = await browser.newPage();
   client = await page.target().createCDPSession();
-  await client.send('Network.enable');
   await testPage(page, client); // only for creating fresh instance
+  await client.send('Network.enable');
   await client.send('Network.clearBrowserCache');
   console.log(await testPage(page, client)); // second enter only with sw
   await browser.close();
